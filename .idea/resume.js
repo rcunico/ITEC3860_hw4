a37c5542e430ca7fc7d742b3f868500bd88da861
RCunico <ryan.cunico@gmail.com>
2019-01-31 22:11:31 -0500

    Small styling changes to CSS / rdc.html / team.html / general cleanup

--- .idea/resume.js
+++ .idea/resume.js
@@ -1,43 +1,51 @@
-
-function hideShow(id) {
+function hideShow(id)
+    {
     var divID = document.getElementById(id);
 
     if (divID.style.display == "none")
-    {
+        {
         divID.style.display = "block";
-    }
+        }
     else
-    {
+        {
         divID.style.display = "none";
+        }
     }
-}
 
-function changeText(button) {
+function changeText(button)
+    {
     var endText = button.innerHTML.substring(4);
-    if (button.innerHTML.substring(0, 4).toString() == "Hide") {
+    if (button.innerHTML.substring(0, 4).toString() == "Hide")
+        {
         button.innerHTML = "Show " + endText;
-    }
-    else {
+        }
+    else
+        {
         button.innerHTML = "Hide " + endText;
+        }
     }
-}
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function menuFunction()
+    {
     document.getElementById("dropdownMenu").classList.toggle("show");
-}
+    }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+window.onclick = function (event)
+    {
+    if (!event.target.matches('.dropbtn'))
+        {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
-        for (i = 0; i < dropdowns.length; i++) {
+        for (i = 0; i < dropdowns.length; i++)
+            {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
+            if (openDropdown.classList.contains('show'))
+                {
                 openDropdown.classList.remove('show');
+                }
             }
         }
     }
-}
